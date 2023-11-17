d5991c28caebcedd329867794dc608e07690a2ff
Théo Dressy <t.dressy@gmail.com>
2023-11-17 15:31:11 +0100

    filters

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -35,3 +35,64 @@ function displayProjects(projects) {
 
     createGallery(); // Appel de la fonction createGallery pour créer la galerie d'images
 }
+
+fetch("http://localhost:5678/api/categories")
+    .then(response => response.json())
+    .then(categories => {
+        const filterButtons = document.querySelector(".filter-buttons");
+
+        //Utilise map pour générer la structure HTML pour chaque catégorie
+        const buttonsHtml = categories.map(category => {
+            return /*html*/`
+            <button class="filter">${category.name}</button>
+            `;
+        }).join('');
+
+        // Ajoute le bouton "Tous" en tant que premier bounton
+        const allButtonHtml = /*html*/ `
+        <button class="filter filter-selected">Tous</button>
+        `;
+
+        // Crée la structure complète en combinant le bouton "Tous" avec les boutons de catégories
+        const filterButtonsHtml = allButtonHtml + buttonsHtml;
+
+        // Utilise innerHTML pour mettre à jour le contenu de la div filter-buttons
+        filterButtons.innerHTML = filterButtonsHtml;
+
+        // Récupère tous les boutons de filtre
+        const buttons = document.querySelectorAll(".filter-buttons button")
+
+        // Ajoute un event listener à chaque bouton de filtre 
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                //Si le bouton "Tous" est cliqué (avec la classe "filter-selected"), on appelle "filterProjects" avec categoryId à null
+                //Sinon, on recherche l'id de la catégorie associée et on appelle "filterProjects" avec et cet id pour filtrer les projets par catégorie.
+                const categoryId = button.classList.contains("filter-selected") ? null : categories.find(category => category.name === button.textContent)?.id;
+                filterProjects(categoryId, button);
+            });
+        });
+    });
+
+function filterProjects(categoryId, selectedButton) {
+    // Vérifie si le bouton actuel est déjà sélectionné
+    const isAlreadySelected = selectedButton.classList.contains("filter-selected");
+
+    // Si le bouton est déjà sélectionné, ne faites rien
+    if (isAlreadySelected) {
+        return;
+    }
+
+    // Sinon, effectuez la logique de filtrage
+    const filteredProjects = !categoryId ? allProjects : allProjects.filter(project => project.categoryId === categoryId);
+    displayProjects(filteredProjects);
+    setSelectedFilter(selectedButton);
+}
+
+
+function setSelectedFilter(selectedButton) {
+    const buttons = document.querySelectorAll(".filter-buttons button");
+    buttons.forEach(button => {
+        button.classList.remove("filter-selected");
+    });
+    selectedButton.classList.add("filter-selected");
+};
