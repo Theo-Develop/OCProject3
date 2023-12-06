b342c0f9af11aee4dfb0bcbe8bb8f9605607f5fb
Théo Dressy <t.dressy@gmail.com>
2023-12-06 09:00:37 +0100

    modale.js delete items

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -25,7 +25,7 @@ document.querySelector("#modal-close").addEventListener("click", (event) => {
 
 // FERMETURE A L'APPUI DE LA TOUCHE ECHAP
 window.addEventListener("keydown", function (event) {
-    if (event.key === "Escape" || e.key === "Esc") {
+    if (event.key === "Escape" || event.key === "Esc") {
         overlayModalContent.style.display = "none";
         modalContent.style.display = "none";
     }
@@ -42,30 +42,81 @@ window.onclick = (event) => {
 // FONCTION AFFICHER LES PROJETS
 function modalProjects(projects) {
     const createModalGallery = () => {
-        const modalGallery = /*html*/ `
-        <div class="modal-gallery">
-            ${projects.map((project) => /*html*/`
-                <figure>
-                    <img src="${project.imageUrl}" alt="${project.title}">
-                    <i class="fa-solid fa-trash-can"></i>
-                </figure>
-            `).join('')}
-        </div>
-        `;
+        const modalGalleryHTML = projects.map((project) => /*html*/ `
+            <figure data-project-id="${project.id}">
+                <img src="${project.imageUrl}" alt="${project.title}">
+                <i class="fa-solid fa-trash-can"></i>
+            </figure>
+        `).join('');
 
         // Cherche la section avec l'id "modal-content"
         const portfolioSection = document.getElementById("modal-content");
 
-        //vérifie s'il y a déjà une galerie, si oui, la remplace
+        // Vérifie s'il y a déjà une galerie, si oui, la remplace
         const existingGallery = document.querySelector(".modal-gallery");
         if (existingGallery) {
-            existingGallery.outerHTML = modalGallery;
+            console.log("Existing gallery found. Replacing content...");
+            existingGallery.innerHTML = modalGalleryHTML;
         } else {
-            //sinon, ajoute la nouvelle galerie à l'intérieur de la section "portfolio"
-            portfolioSection.insertAdjacentHTML("beforeend", modalGallery);
+            console.log("No existing gallery. Adding new gallery...");
+            // Sinon, ajoute la nouvelle galerie à l'intérieur de la section "portfolio"
+            portfolioSection.insertAdjacentHTML("beforeend", `<div class="modal-gallery">${modalGalleryHTML}</div>`);
         }
     };
 
     createModalGallery(); // Appel de la fonction createModalGallery pour créer la galerie d'images
+
+    // Sélectionne toutes les figures de la galerie
+    const allFigures = document.querySelectorAll(".modal-gallery figure");
+
+    allFigures.forEach((figure) => {
+        // Écoute du click sur une corbeille pour supprimer un élément
+        const deleteIcon = figure.querySelector(".fa-trash-can");
+
+        figure.addEventListener("click", (event) => {
+            const projectId = figure.getAttribute("data-project-id");
+            if (deleteIcon && event.target === deleteIcon) {
+                console.log("Delete icon clicked for project ID:", projectId);
+                event.preventDefault();
+                deleteWork(projectId, figure.querySelector("img").alt, localStorage.getItem("token"));
+            }
+        });
+
+        console.log("Figure added to click event listener:", figure);
+    });
 }
+
+// SUPPRIMER PROJETS
+function deleteWork(projectId, projectTitle, token) {
+    const deleteConfirm = window.confirm(`Êtes-vous sûr de vouloir supprimer le projet : ${projectTitle} ?`);
+    if (deleteConfirm) {
+        const fetchDelete = fetch(`${"http://localhost:5678/api/works"}/${projectId}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        fetchDelete.then((response) => {
+            if (response.ok) {
+                msgDeleteOkF();
+                modalProjects(allProjects);
+            } else {
+                // MESSAGE DE SUPPRESSION OK
+            }
+        });
+    }
+}
+
+const msgDeleteOkSlot = document.querySelector(".msg-delete-ok");
+const msgDeleteOk = "Le projet a bien été supprimé.";
+
+function msgDeleteOkF() {
+    msgDeleteOkSlot.textContent = msgDeleteOk;
+
+    setTimeout(() => {
+        msgDeleteOkSlot.textContent = "";
+    }, 3000);
+}
+
 
