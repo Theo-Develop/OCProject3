87746d4eedf43fa5f582d1f37e293828b7daa6d2
Théo Dressy <t.dressy@gmail.com>
2023-12-12 10:54:18 +0100

    settings adding new files in Database

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -154,18 +154,174 @@ document.querySelector("#modal-photo-close").addEventListener("click", (event) =
 })
 
 // FONCTION AFFICHER LES CATEGORIES DANS LA MODAL PHOTO
+categorySelect = document.querySelector("#modal-photo-category");
 const showCategoriesModal = () => {
-    const categorySelect = document.querySelector("#modal-photo-category");
     categorySelect.innerHTML = "<option value='' hidden></option>";
 
     // Utilisez les catégories récupérées dans initialise
     allCategories.forEach(category => {
         const option = document.createElement("option");
         option.setAttribute("value", category.id);
-        option.textContent = category.name; // Assurez-vous d'avoir la propriété correcte pour le nom de la catégorie
+        option.textContent = category.name;
         categorySelect.appendChild(option);
     });
 };
+
+// AJOUTER PROJETS
+const btnNewPhoto = document.querySelector("#image");
+const photoBox = document.querySelector("#form-photo");
+const allContentPhotoBox = photoBox.querySelectorAll(".display-none");
+
+btnNewPhoto.addEventListener("change", (e) => {
+    e.preventDefault();
+    const objectURL = URL.createObjectURL(btnNewPhoto.files[0]);
+    allContentPhotoBox.forEach((content) => {
+        content.style.display = "none";
+    })
+    const ajoutImage = document.createElement("img");
+    ajoutImage.setAttribute("src", objectURL);
+    ajoutImage.setAttribute("alt", btnNewPhoto.files[0].name);
+    photoBox.appendChild(ajoutImage);
+    checkForm()
+})
+
+
+// VERIFICATION FORMULAIRE COMPLET
+const modalPhotoTitle = document.querySelector("#modal-photo-title");
+const btnFormValider = document.querySelector(".modal-valider");
+function checkForm() {
+    if (modalPhotoTitle.value !== "" && btnNewPhoto.files[0] !== undefined && categorySelect.value !== "") {
+        btnFormValider.classList.add("form-btn-valider")
+        const btnFormValiderCheck = document.querySelector(".form-btn-valider");
+        btnFormValiderCheck.addEventListener("click", sendWork);
+    }
+}
+
+// RESET FORMULAIRE
+function resetForm() {
+    document.querySelector("#modal-photo-title").value = "";
+    imageInput.value = "";
+    allContentPhotoBox.forEach((content) => {
+        content.style.display = "block";
+        if (document.querySelector("#form-photo img")) {
+            document.querySelector("#form-photo img").remove();
+            btnFormValider.classList.remove("form-btn-valider")
+        };
+    })
+}
+
+let categorySelectId = "";
+categorySelect.addEventListener("change", () => {
+    categorySelectId = categorySelect.selectedIndex;
+    checkForm();
+})
+
+modalPhotoTitle.addEventListener("change", () => {
+    checkForm();
+})
+
+// AJOUTER DES PROJETS
+const msgSuccesErrorSlot = document.querySelector(".msg-add-photo-success-error");
+const msgBadSizeFormatImg = document.querySelector(".msg-bad-size-format-img");
+
+// FONCTION VERIFICATION TAILLE ET FORMAT IMAGE
+const imageInput = document.querySelector("#image");
+imageInput.addEventListener("change",
+    function checkImg() {
+        const selectedImage = imageInput.files[0];
+
+        if (selectedImage) {
+            if (selectedImage.size > 4 * 1024 * 1024) {
+                resetForm();
+                msgBadSize();
+                return;
+            }
+
+            const allowedFormats = ["image/jpeg", "image/png"];
+            if (!allowedFormats.includes(selectedImage.type)) {
+                resetForm();
+                msgBadFormat();
+                return;
+            }
+        }
+    });
+
+// FONCTION DE MESSAGE DE SUCCES AJOUT DE PROJET
+const msgAddSuccess = "Projet ajouté avec succés !";
+function msgAddSuccessF() {
+    msgSuccesErrorSlot.textContent = msgAddSuccess;
+    msgSuccesErrorSlot.style.display = "block";
+
+    setTimeout(() => {
+        msgSuccesErrorSlot.textContent = "";
+        msgSuccesErrorSlot.style.display = "none";
+    }, 3000);
+}
+
+// FONCTION DE MESSAGE ERREUR AJOUT DE PROJET
+const msgAddError = "Un problème est survenue, veuillez recommencer.";
+function msgAddErrorF() {
+    msgSuccesErrorSlot.textContent = msgAddError;
+    msgSuccesErrorSlot.style.display = "block";
+
+    setTimeout(() => {
+        msgSuccesErrorSlot.textContent = "";
+        msgSuccesErrorSlot.style.display = "none";
+    }, 3000);
+}
+
+// FONCTION MESSAGE ERREUR POUR IMAGE TROP LOURDE
+const errorBadSize = "L'image dépasse la limite de taille de 4 Mo !";
+function msgBadSize() {
+    msgBadSizeFormatImg.textContent = errorBadSize;
+
+    setTimeout(() => {
+        msgBadSizeFormatImg.textContent = "";
+    }, 3000);
+}
+
+// FONCTION MESSAGE ERREUR POUR MAUVAIS FORMAT IMAGE
+const errorBadFormat = "Format de fichier non supporté. Utilisez JPEG ou PNG.";
+function msgBadFormat() {
+    msgBadSizeFormatImg.textContent = errorBadFormat;
+
+    setTimeout(() => {
+        msgBadSizeFormatImg.textContent = "";
+    }, 3000);
+}
+
+
+// FONCTION AJOUT DE PROJETS
+const token = localStorage.getItem("token")
+
+const sendWork = async (event) => {
+    event.preventDefault();
+    const image = document.querySelector("#image").files[0];
+    const title = document.querySelector("#modal-photo-title").value;
+
+    let formData = new FormData();
+
+    formData.append("image", image);
+    formData.append("title", title);
+    formData.append("category", categorySelectId);
+
+    const response = await fetch("http://localhost:5678/api/works/", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+        },
+        body: formData
+    });
+
+    if (response.ok) {
+        msgAddSuccessF();
+        resetForm();
+        modalProjects();
+    } else {
+        msgAddErrorF();
+    }
+}
+
 
 
 
